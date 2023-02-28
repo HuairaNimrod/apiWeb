ff9d885053442e73499fd4857a33ab72acbfcf25
Huaira Nimrod <nimax29@gmail.com>
2023-02-28 04:52:09 -0700

    test api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-const routes  = require('express').Router();
-const myControllers  = require('../controllers');
+// const routes  = require('express').Router();
+// const myControllers  = require('../controllers');
+const express = require('express');
+const router = express.Router(); // line 1 is a simplified version of this
 
+router.use('/contacts', require('./contacts'))
 
-routes.get('/', myControllers.awesomeFunction);
-routes.get('/awesome', myControllers.returnAnotherPerson);
+// routes.get('/', myControllers.awesomeFunction);
+// routes.get('/awesome', myControllers.returnAnotherPerson);
 
-module.exports = routes;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000
-app.use('/', require('./routes'));
+// var express = require('express');
+// var app = express();
+// const port = process.env.PORT || 3000
+// app.use('/', require('./routes'));
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-}); 
+// app.listen(port, ()=>{
+//     console.log(`Server is running on port ${port}`);
+// }); 
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
